d005c0ef464c1219542199ffba7e87a638cc739f
Nghia Dang <vannghiadang79@gmail.com>
2025-04-30 20:51:23 +0700

    Initial commit - Next.js + Tailwind + shadcn/ui setup

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,62 @@
+import { Home, Newspaper, Tag, StickyNote } from "lucide-react"
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+    {
+        title: "Dashboard",
+        url: "/dashboard",
+        icon: Home,
+    },
+    {
+        title: "Articles",
+        url: "/dashboard/articles",
+        icon: Newspaper,
+    },
+    {
+        title: "Categories",
+        url: "/dashboard/categories",
+        icon: StickyNote,
+    },
+    {
+        title: "Tags",
+        url: "/dashboard/tags",
+        icon: Tag,
+    }
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>ContentBoard</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                        {items.map((item) => (
+                            <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton asChild>
+                                <a href={item.url}>
+                                    <item.icon />
+                                    <span>{item.title}</span>
+                                </a>
+                            </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
